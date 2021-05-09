0f103b85b57f4e7238fe09cc5e9bad21eda51c6e
T.  Ramjee <tramjee@gmail.com>
2021-05-09 13:17:17 -0500

    Added customer endpoint

---
+++ src/main/java/guru/springframework/msscbrewery/services/CustomerServiceImpl.java
@@ -0,0 +1,16 @@
+package guru.springframework.msscbrewery.services;
+
+import guru.springframework.msscbrewery.web.model.CustomerDto;
+import org.springframework.stereotype.Service;
+
+import java.util.UUID;
+
+@Service
+public class CustomerServiceImpl implements CustomerService {
+    @Override
+    public CustomerDto getCustomerById(UUID customerId) {
+        return CustomerDto.builder().id(UUID.randomUUID())
+                .customerName("Test Customer")
+                .build();
+    }
+}
